f5ea446d6be6dc87b8baa75ee54585fbf353b881
antonxuanquang <antonxuanquang@yahoo.com>
2016-10-01 20:58:21 -0500

    change header look

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	'secretKey': '12345‐67890‐09876‐54321',
-	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	'mongoUrl': 'mongodb://localhost:27017/dishes',
 	'facebook': {
 		clientID: '1114906558583574',
 		clientSecret: '240cafbd334fa1d1104ff5b14db23334',
-		callbackURL: 'https://localhost:3443/users/facebook/callback'
+		callbackURL: 'https://localhost/users/facebook/callback'
 	}
 }
